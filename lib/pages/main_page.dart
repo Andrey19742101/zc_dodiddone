00617179255167dbd82994ceb969f8e5184390d1
Andrey Matyushenko <matyushenko.andr@gmail.com>
2024-07-26 16:57:53 +0300

    floating action button

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-
-import 'package:zc_dodiddone/screens/profile.dart';
+import 'package:intl/intl.dart';
+import 'package:zc_dodiddone/screens/all_tasks.dart';
 import 'package:zc_dodiddone/theme/theme.dart';
-
-import '../screens/all_tasks.dart';
+import 'package:zc_dodiddone/screens/profile.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
@@ -17,7 +16,6 @@ class _MainPageState extends State<MainPage> {
 
   static const List<Widget> _widgetOptions = <Widget>[
     TasksPage(),
-    Text('Задачи'),
     Text('Сегодня'),
     Text('Выполнено'),
     ProfilePage(), // Заменяем Text на ProfilePage
@@ -26,6 +24,118 @@ class _MainPageState extends State<MainPage> {
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
+    });
+  }
+
+  // Функция для открытия диалогового окна
+  void _showAddTaskDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        String title = '';
+        String description = '';
+        DateTime deadline = DateTime.now();
+
+        return Dialog(
+          // Используем Dialog вместо AlertDialog
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16), // Скругленные углы
+          ),
+          child: Container(
+            width: 400, // Ширина диалогового окна
+            padding: const EdgeInsets.all(20), // Отступ для содержимого
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  onChanged: (value) {
+                    title = value;
+                  },
+                  decoration: const InputDecoration(
+                    labelText: 'Название задачи',
+                  ),
+                ),
+                const SizedBox(height: 16),
+                TextField(
+                  onChanged: (value) {
+                    description = value;
+                  },
+                  decoration: const InputDecoration(
+                    labelText: 'Описание',
+                  ),
+                ),
+                const SizedBox(height: 16),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 16), // Отступ для кнопки
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Открыть календарь для выбора даты и времени
+                      showDateTimePicker(context).then((selectedDateTime) {
+                        if (selectedDateTime != null) {
+                          deadline = selectedDateTime;
+                        }
+                      });
+                    },
+                    child: const Text('Выбрать дедлайн'),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                Text(
+                    'Дедлайн: ${DateFormat('dd.MM.yy HH:mm').format(deadline)}'),
+                const SizedBox(height: 24),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end, // Кнопки справа
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text('Отмена'),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        // Обработка добавления задачи
+                        // Например, можно добавить задачу в список задач
+                        // и закрыть диалоговое окно
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text('Добавить'),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  // Функция для показа диалога выбора даты и времени
+  Future<DateTime?> showDateTimePicker(BuildContext context) async {
+    return await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime.now(),
+      lastDate: DateTime(2100),
+    ).then((pickedDate) {
+      if (pickedDate == null) return null;
+
+      return showTimePicker(
+        context: context,
+        initialTime: TimeOfDay.fromDateTime(pickedDate),
+      ).then((pickedTime) {
+        if (pickedTime == null) return null;
+
+        return DateTime(
+          pickedDate.year,
+          pickedDate.month,
+          pickedDate.day,
+          pickedTime.hour,
+          pickedTime.minute,
+        );
+      });
     });
   }
 
@@ -73,6 +183,10 @@ class _MainPageState extends State<MainPage> {
         ],
         currentIndex: _selectedIndex,
         onTap: _onItemTapped,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _showAddTaskDialog,
+        child: const Icon(Icons.add),
       ),
     );
   }
